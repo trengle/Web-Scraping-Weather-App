43fdf04bfd231eb76210800f9166f7a0e333cba0
tyler <TylerEngleVFX@Gmail.com>
2025-03-30 19:05:56 -0700

    Deals with bad format input by user and parses HTML tags cleaner, and uses env file. Complete.

--- weather_parsing.py
+++ weather_parsing.py
@@ -2,30 +2,26 @@ import urllib.request
 from bs4 import BeautifulSoup
 import re
 import pprint
-import webbrowser
+import os
+from dotenv import load_dotenv
 
-api_key="your_api_key_here" # Replace with your actual API key
-address = input("Enter location in the format 'City,State' : ")
-# address = "Carmel,Indiana"
-#Carmel,+IN
+'''
+The user will need to create a .env file in the same directory as this script with the following content:
+API_KEY=your_api_key
+'''
+
+load_dotenv()
+api_key=str(os.getenv("API_KEY")) # Replace with your actual API key
+# address = "Carmel,IN"
+address = input("Enter location in the format 'City,State' : ").replace(" ", "").title()
 base_url = f"https://geocode.maps.co/search?q={address}&api_key={api_key}"
-#params = {"query": inp}
-#search_url = f"{base_url}{inp}"
-# Open the URL in the default web browser
-#webbrowser.open(search_url)
 
 html1 = urllib.request.urlopen(base_url).read()
 soup = BeautifulSoup(html1, 'html.parser')
-# print(search_url)
+
 stroup1 = str(soup)
 splitStroup=(stroup1.split('"') )
-index = -1
-# for i in splitStroup:
-    # if i.startswith("lon"):
-        # index = splitStroup.index(i) #23
-        # lat = splitStroup[index+2]
-        # lon = splitStroup[index+2] #27
-        # print(lon, index )
+
 lat = splitStroup[23+2]
 lon = splitStroup[27+2]
 weather_url = "https://weather.com/weather/today/l/"
@@ -35,12 +31,10 @@ html2 = urllib.request.urlopen(search_url).read()
 soup2 = BeautifulSoup(html2, 'html.parser')
 stroup2 = str(soup2)
 
-# print(stroup2)
+#Fancy Soup parsing:
+temp_span = soup2.find("span", {"data-testid": "TemperatureValue"})
+# Extract the temperature value (text) from the span
+temperature_value = temp_span.text.strip()
+#Prints final result:
+print(f"{address} is {temperature_value} degrees F.")
 
-found = stroup2.find('<span>°</span><span></span></span></span></div><div id="SunriseSunsetCon')
-extline = stroup2[found-2:found+0]
-print(f"{address} is {extline} degrees F.")
-
-# found = stroup2.find('"ltr">67<span>°')
-# extline = stroup2[found+6:found+8]
-# print(f"{address} is {extline} degrees F.")
